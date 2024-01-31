87675fbe3c6c56524921594af09cd73af74fd38d
Douglas <douglaswlc@gmail.com>
2024-01-31 18:34:25 -0300

    subindo tecnologias e ajustes

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
 async function fetchProfileData() {
   const url =
-    "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json";
-  const fetching = await fetch(url);
-  return await fetching.json();
+    "https://raw.githubusercontent.com/douglaswlc/my-portfolio/initial-project/data/profile.json";
+  const response = await fetch(url);
+  const profileData = await response.json();
+  return profileData;
 }
